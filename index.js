7d5dc8c55bf40ce8a1de01e30405fd0b76062085
Evgeniy Kozhich <wanderbon95@gmail.com>
2020-02-03 10:38:48 +0300

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+import FluidNavigator from './lib/FluidNavigator';
+import createFluidNavigator from './lib/createFluidNavigator';
+import Transition from './lib/TransitionView';
+
+export { RouteDirection, Metrics, TransitionSpecification } from './lib/Types';
+export { FluidNavigator, createFluidNavigator, Transition };
